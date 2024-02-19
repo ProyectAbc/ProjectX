d6e6ca0d7c755d315a75c2f13cd220fd122e6395
Christian Cachero <Ch_canchero@live.com.ar>
2024-02-19 17:32:09 -0300

    Se agregó dependencias actuator, y un endpoint de prueba para comprobarlo. Todo esto para hacer funcionar y comprobar el correcto funcionamiento de RefreshScope

--- Rest-Api 1/src/main/java/com/projectx/controller/PersonaController.java
+++ Rest-Api 1/src/main/java/com/projectx/controller/PersonaController.java
@@ -1,13 +1,27 @@
 package com.projectx.controller;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
+@Getter
+@Setter
+@RefreshScope
 public class PersonaController {
 
+    @Value("${app.testApi}")
+    private String testApi;
+
+    @GetMapping("test-api")
+    public String getTestApi(){
+        return this.testApi;
+    }
     @GetMapping("/hello")
     public String TestHello(){
         return "Hola que tal";
